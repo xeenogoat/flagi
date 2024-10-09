e289e3165c40f999ce00dd0af478b57efd8dfb3f
xeenogoat <07kamilos@gmail.com>
2024-10-09 15:16:17 +0200

    costam

--- app/src/main/java/com/example/flaga/MainActivity.java
+++ app/src/main/java/com/example/flaga/MainActivity.java
@@ -4,17 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int licznikZlych = 0;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewPolecenie);
     }
 
     public void zlaOdpowiedz(View view) {
-        Toast.makeText(this, "to jest kolor flagi Polski", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.podsumowanie, Toast.LENGTH_SHORT).show();
+    }
+
+    public void dobraOdpowiedz(View view) {
+        view.setVisibility(view.INVISIBLE);
+        licznikZlych++;
+        if(licznikZlych ==4){
+            textView.setText(R.string.podsumowanie);
+        }
     }
 }
